f91aa553ec9ffbc27ad9c1dc242e6cf70fa620a3
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-15 18:36:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? ' ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'put id' : process.env.SESSION_ID,
 BTN: "FOLLOW US :)",
 FOOTER: "*ꜱʜᴀᴅᴏᴡ-ᴍᴅ-ᴠᴇʀꜱɪᴏɴ 6➤*",
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
