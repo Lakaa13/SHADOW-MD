66c8354e1eb077574bd9d2b763e6e036007f8c98
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-15 23:09:41 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -40,7 +40,6 @@ let buttons = [{
                         id: ".hiru"
                 }),
             },
-    },
     ]
         let opts = {
             image: `https://i.imgur.com/NAK3YWD.jpeg`,
