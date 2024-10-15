8b473d8a68595822499e628326ec430a967af28a
Sadaras200 <123821282+Sadaras200@users.noreply.github.com>
2024-10-15 15:09:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
-SUDO_NB: process.env.SUDO_NB || "94767910958",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_BIO: process.env.AUTO_BIO || "false",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "unavailable",
-AUTO_READ_CMD: process.env.AUTO_READ_CMD || "false",
-AUTO_TYPING:"true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE:"true",
+SESSION_ID: process.env.SESSION_ID === undefined ? ' ' : process.env.SESSION_ID,
+BTN: "FOLLOW US :)",
+FOOTER: "*ᴘᴏᴡᴇʀᴅ ʙʏ ᴅᴀʀᴋꜱᴀᴅᴀꜱ*",
+
+
+
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
+
+MAX_SIZE: 200,
+
 };
