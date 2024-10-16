a5eae9e8008c26572250bebcfb8b32e0ca1ca6aa
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-17 00:50:14 +0530

    Update grup2.js

--- plugins/grup2.js
+++ plugins/grup2.js
@@ -1,5 +1,7 @@
 const config = require('../config')
 const { cmd, commands } = require('../command')
+const { default: makeWASocket, useMultiFileAuthState, WA_DEFAULT_EPHEMERAL, jidNormalizedUser, proto, getDevice, generateWAMessageFromContent, fetchLatestBaileysVersion, makeInMemoryStore, getContentType, generateForwardMessageContent, downloadContentFromMessage, jidDecode } = require('@whiskeysockets/baileys')
+//-----------
 
 cmd({
     pattern: "mute",
