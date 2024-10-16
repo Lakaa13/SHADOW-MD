03653edd55645e23245423f213e57c3215df6308
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-17 00:33:32 +0530

    Update grup.js

--- plugins/grup.js
+++ plugins/grup.js
@@ -561,3 +561,263 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         reply(`${e}`)
     }
 })
+
+cmd({
+    pattern: "mute",
+    react: "🔒",
+    desc: "close a group",
+    category: "group",
+    use: '.mute',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+    
+    if (!isGroup) return reply('This command can only be used in a group❗.')
+        if (!isBotAdmins) return reply('කරුනාකර ශැඩො md බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.')
+ 
+                                  
+        await conn.groupSettingUpdate(mek.chat, 'announcement')
+        const sendmsg = await conn.sendMessage(mek.chat.G_MUTE)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('🔒 GROUP IS CLOSED MY BOT OWNER')
+l(e)
+}
+})
+
+
+  
+cmd({
+    pattern: "unmute",
+    react: "🔓",
+    desc: "open a group",
+    category: "group",
+    use: '.unmute',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+    
+    if (!isGroup) return reply('This command can only be used in a group❗')
+        if (!isBotAdmins) return reply('කරුනාකර ශැඩො md බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗.')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.')
+ 
+                                  
+        await conn.groupSettingUpdate(mek.chat, 'not_announcement')
+        const sendmsg = await conn.sendMessage(mek.chat.G_UNMUTE)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('🔓 GROUP IS OPEN MY BOT OWNER')
+l(e)
+}
+})
+
+
+cmd({
+    pattern: "promote",
+    react: "🔖",
+    desc: "promote admin to a member",
+    category: "group",
+    use: '.promote',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+    
+    if (!isGroup) return reply('This command can only be used in a group❗.')
+        if (!isBotAdmins) return reply('කරුනාකර ශැඩො md බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗.')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.')
+ 
+                                  
+         let users = mek.mentionedJid ? mek.mentionedJid : mek.quoted ? mek.quoted.sender : q.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+		await conn.groupParticipantsUpdate(mek.chat, [users], 'promote').then((res) => reply(jsonformat(res))).catch((err) => reply(jsonformat(err)))
+	reply('✅ GROUP ADDMIN PROMOTE BY BOT OWNER')
+await conn.sendMessage(from, { react: { text: `💫`, key: mek.key }}) 
+} catch (e) {
+reply('*කරුනාකර පසුව උත්සකා කරන්න.❌*')
+l(e)
+}
+}) 
+
+
+cmd({
+    pattern: "demote",
+    react: "🔖",
+    desc: "demote admin to a member",
+    category: "group",
+    use: '.demote',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+    
+    if (!isGroup) return reply('This command can only be used in a group❗.')
+        if (!isBotAdmins) return reply('කරුනාකර ශැඩො md බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗.')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.')
+ 
+                                  
+         	let users = mek.mentionedJid ? mek.mentionedJid : mek.quoted ? mek.quoted.sender : q.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+		await conn.groupParticipantsUpdate(mek.chat, [users], 'demote').then((res) => reply(jsonformat(res))).catch((err) => reply(jsonformat(err)))
+reply('🔓 GROUP IS OPEN MY BOT OWNER')
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*කරුනාකර පසුව උත්සහ කරන්න ❌*')
+l(e)
+}
+})
+
+
+cmd({
+pattern: "del",
+react: "❌",
+alias: [","],
+desc: "delete message",
+category: "group",
+use: '.del',
+filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+if (!isOwner ||  !isAdmins) return;
+try{
+if (!m.quoted) return reply(mg.notextfordel);
+const key = {
+            remoteJid: m.chat,
+            fromMe: false,
+            id: m.quoted.id,
+            participant: m.quoted.sender
+        }
+        await conn.sendMessage(m.chat, { delete: key })
+} catch(e) {
+console.log(e);
+reply('Error!!')
+} 
+})
+
+cmd({
+    pattern: "add",
+    desc: "Add a member to the group.",
+    category: "group",
+    react: "💫",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+            
+    if (!isGroup) return reply('This command can only be used in a group❗.')
+        if (!isBotAdmins) return reply('කරුනාකර ශැඩො md බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗.')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.')
+ 
+
+        const user = q.split(' ')[0]
+        if (!user) return reply('Please provide a phone number to add.')
+
+        await conn.groupParticipantsUpdate(from, [`${user}@s.whatsapp.net`], 'add')
+        await reply(`@${user} has been added to the group.`, { mentions: [`${user}@s.whatsapp.net`] })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
+
+cmd({
+    pattern: "kickall",
+    react: "🔖",
+    desc: "To kick all members in one time",
+    category: "group",
+    use: '.kickall',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{   
+if (!isGroup) return reply(ONLGROUP ONLY)
+if (!isAdmins) return reply(ADMIN ONLY Command )	
+for (let mem of participants) {
+                      
+                      await sleep(1000)
+                      if(mem.id == botNumber+'@s.whatsapp.net') return
+                      if(mem.id == owner+'@s.whatsapp.net') return
+                      await conn.groupParticipantsUpdate(from, [mem.id], 'remove')
+                      await conn.sendText(from,`*${mem.id.split('@')[0]} Kick out !!!*`)
+                                  }
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+}) 		    	
+//==============
+cmd({
+    pattern: "remove",
+    desc: "Remove a member from the group.",
+    category: "group",
+    react: "🚫",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) return reply('This command can only be used in a group.')
+        if (!isBotAdmins) return reply('Bot must be an admin to use this command.')
+        if (!isAdmins) return reply('You must be an admin to use this command.')
+
+        const user = m.mentioned[0] || m.quoted?.sender
+        if (!user) return reply('Please tag or reply to a user to remove.')
+
+        await conn.groupParticipantsUpdate(from, [user], 'remove')
+        await reply(`@${user.split('@')[0]} has been removed from the group.`, { mentions: [user] })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
+//=======
+
+cmd({
+    pattern: "setgoodbye",
+    desc: "Set the goodbye message for the group.",
+    category: "group",
+    react: "👋",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) return reply('This command can only be used in a group.')
+        if (!isBotAdmins) return reply('Bot must be an admin to use this command.')
+        if (!isAdmins) return reply('You must be an admin to use this command.')
+
+        const goodbye = q
+        if (!goodbye) return reply('HI WELCOME NEW MEMBAR💗👨‍💻.')
+
+        await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: goodbye })
+        await reply('Goodbye message has been set.')
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
+
+
+cmd({
+    pattern: "setwelcome",
+    desc: "Set the welcome message for the group.",
+    category: "group",
+    react: "👋",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) return reply('This command can only be used in a group.')
+        if (!isBotAdmins) return reply('Bot must be an admin to use this command.')
+        if (!isAdmins) return reply('You must be an admin to use this command.')
+
+        const welcome = q
+        if (!welcome) return reply('BY  I MIS YOU PLEASE COME BACK 💗👨‍💻.')
+
+        await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: welcome })
+        await reply('Welcome message has been set.')
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
