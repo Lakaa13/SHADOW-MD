215dbed8d11674877570682f1ab92113db12019e
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-16 08:09:29 +0530

    Create news.js

---
+++ plugins/news.js
@@ -0,0 +1,58 @@
+const config = require('../config')
+const { cmd } = require('../command')
+const axios = require('axios')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://dark-yasiya-news-apis.vercel.app/api' // API LINK ( DO NOT CHANGE THIS!! )
+
+
+// ================================HIRU NEWS========================================
+
+cmd({
+    pattern: "news",
+    alias: ["hiru","news1"],
+    react: "💭",
+    desc: "",
+    category: "news",
+    use: '.hirunews',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted }) => {
+try{
+
+const news = await fetchJson(`${apilink}/hiru`)
+  
+const msg = `
+           🌞 *HIRU NEWS* 🌞
+
+       
+➤ *Title* - ${news.result.title}
+
+➤ *News* - ${news.result.desc}
+
+➤ *Link* - ${news.result.url}`
+
+let buttons = [{
+            
+                name: "NEWS",
+                buttonParamsJson: JSON.stringify({
+                    display_text: "NEWS",
+                    id: ".hiru"
+                }),
+            },
+  {
+    }
+            ]
+            let message = {
+                image: '',
+                header: '',
+                footer: config.FOOTER,
+                body: msg
+
+            }
+            return await conn.sendButtonMessage(from, buttons, m, message)
+        } catch (e) {
+            console.log(e)
+            reply('*Error !!*')
+        }
+    });
