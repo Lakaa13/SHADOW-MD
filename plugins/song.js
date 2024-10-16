fe4642c3a6a386dab0fe64e5bc39d15b4cc8268b
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-16 21:21:15 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -16,7 +16,7 @@ cmd({
     desc: "Search & download yt song.",
     category: "download",
     filename: __filename
-}, async (conn, mek, m, { from, quoted, body, prefix, isCmd, command, args, q }) => {
+}, async (conn, mek, m, { from, quoted, body, prefix, isCmd, command, args, reply, q }) => {
     try {
         if (!q) return reply("Please provide a song name.");
         
