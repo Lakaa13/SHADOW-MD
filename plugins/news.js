723b72219d9e7f95b2306db6fd40835e5fdc1f0a
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-16 08:45:39 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -75,31 +75,3 @@ const msg = `
         console.log(e)
     }
 })
-
-        //await conn.sendMessage(from, { text: commandList }, { quoted: mek });
-        await conn.sendMessage(from, {
-text: commandList,
-  contextInfo: {
-    mentionedJid: [ '' ],
-    groupMentions: [],
-    forwardingScore: 1111,
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: '120363290448968237@newsletter',
-      serverMessageId: 127
-    },
-externalAdReply: { 
-title: 'ꜱʜᴀᴅᴏᴡ ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ⚟',
-body: 'ᴀ ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ',
-mediaType: 1,
-sourceUrl: "https://whatsapp.com/channel/0029Val6g7EBadmagKxuYi1R" ,
-thumbnailUrl: `https://i.imgur.com/2p7gHUD.jpeg` ,
-renderLargerThumbnail: true,
-showAdAttribution: false
-}
-}}, { quoted: mek})
-    } catch (e) {
-        reply('*Error !!*')
-        console.log(e)
-    }
-})
