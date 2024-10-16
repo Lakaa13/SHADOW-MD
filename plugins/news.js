099f3e2304a161b0a38cbbfebf07aa3abedb9149
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-16 22:16:56 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -31,29 +31,7 @@ const status = `
 ➤ *News* - ${news.result.desc}
 
 ➤ *Link* - ${news.result.url}`
-let buttons = [
-{
-name: "quick_reply",
-buttonParamsJson: JSON.stringify({
-display_text: 'Get Menu',
-id: ".menu"
-}),
-}
- ]
 
-        let opts = {
-            image: `https://i.imgur.com/NAK3YWD.jpeg`,
-            header: "SHADOW-MD-NEWS.LK",
-            footer: config.FOOTER,
-            body: status
-        }
-
-        return await conn.sendButtonMessage(from, buttons, m, opts)
-    } catch (e) {
-        reply('*Error !!*')
-        console.log(e)
-    }
-})
 //========sirasanews==========
 cmd({
     pattern: "sirasanews",
