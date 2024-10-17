0cddb96c2d50744b092677fbaa1b39141d3f29d0
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-17 10:49:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,16 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'put id' : process.env.SESSION_ID,
-AUTO_BIO:  process.env.AUTO_BIO  || false  ,
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,
-WORK_TYPE: process.env.WORK_TYPE || 'public' ,
-AUTO_TYPING:  process.env.AUTO_TYPING  || true  ,
-AUTO_VOICE:  process.env.AUTO_VOICE  || true  ,
-OWNER_REACT:  process.env.OWNER_REACT  || true  ,
+AUTO_BIO: process.env.AUTO_BIO || "false",
+ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "unavailable",
+AUTO_READ_CMD: process.env.AUTO_READ_CMD || "false",
+AUTO_TYPING:"true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true",
+OWNER_REACT:  process.env.OWNER_REACT  || "true",
 BTN: "FOLLOW US :)",
 FOOTER: "*ꜱʜᴀᴅᴏᴡ-ᴍᴅ-ᴠᴇʀꜱɪᴏɴ 6 ➤*",
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
