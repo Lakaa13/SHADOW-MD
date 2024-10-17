a39eca5bebff23d2593276c087306c8a33b23488
Lakshanteach <damayanthalakshan8@gmail.com>
2024-10-17 10:35:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.img
             mek = mek.messages[0]
             if (!mek.message) return
             mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-            if (mek.key && mek.key.remoteJid === 'status@broadcast') && config.AUTO_READ_STATUS === "true"){
+            if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
             await conn.readMessages([mek.key])
             }
             //=============autobio==============
