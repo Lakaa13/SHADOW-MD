1093f805fa6b8f416c65dd4c310bf5b034b96d92
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-10-19 19:43:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put id",
+SESSION_ID: process.env.SESSION_ID || 'put id',
 SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
