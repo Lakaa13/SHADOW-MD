8d7668a637f3f90d6a4da423cb0f7e47457b95d9
Lakshan Damayantha <damayanthalakshan8@gmail.com>
2024-10-20 11:07:35 +0530

    Create xnxx.js

---
+++ plugins/xnxx.js
@@ -0,0 +1,43 @@
+const { cmd, commands } = require('../command');
+const xnxx = require("xnxx-dl");
+const { fetchJson, getBuffer } = require('../lib/functions');
+
+// XNXX video download command
+cmd({
+    pattern: "xnxx",
+    desc: "Downloads a video from XNXX",
+    use: ".xnxx <search_term>",
+    react: "🤤",
+    category: "downloads",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, q, reply }) => {
+    const searchTerm = q.trim();
+    if (!searchTerm) return reply(`Please provide a search term`);
+
+    reply(`Searching For Your Video in shadow md bot ➤...`);
+    try {
+        // Search for the video and download
+        const videoInfo = await xnxx.download(searchTerm);
+        if (!videoInfo || !videoInfo.link_dl) {
+            return await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
+        }
+
+        reply(`Downloading video please waite ➤...`);
+        const videoUrl = videoInfo.link_dl;
+        await conn.sendMessage(
+            from,
+            { video: { url: videoUrl }, caption: '*shadow-md xnxx.com ➤*', mimetype: 'video/mp4' }, 
+            { quoted: mek }
+        );
+
+        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } });
+
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
+        reply(`Error: ${e.message}`);
+    }
+});
+
+module.exports = {};
